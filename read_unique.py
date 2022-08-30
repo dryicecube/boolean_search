498bd37506c81c8a9d4616604516d64440c8f775
dryicecube <ashutosh.lunabotics@gmail.com>
2022-08-30 21:38:28 +0530

    This code counts the unique elements generated from Digital Annealer

---
+++ read_unique.py
@@ -0,0 +1,44 @@
+#Ashutosh Kumar Singh
+#This code returns the number of unique elements in the list for boolean search
+#
+#Date: 30th Aug'22
+
+
+
+import csv
+import time
+import os
+import itertools
+temp=[]
+store=[]
+new_k = []
+n=4                                             #split_the_1st_row_in
+N=50						#number of iterations for data set
+def divide_chunks(l, n):
+     
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]      
+
+one=time.time()
+for i in range(N):
+    with open('data/spins_2x4_run_{}_.txt'.format(i)) as tsv:
+        temp = [x.strip().split('\t') for x in tsv]
+    temp[0] = [ int(x) for x in temp[0] ]
+    
+    a=list(divide_chunks(temp[0], n))
+    #print(*a, sep = "\n")
+    
+    with open('data.txt','a') as wr:
+        for lines in a:
+            store.append(lines)
+            wr.write(f"{lines}\n")
+two=time.time()
+for elem in store:
+    if elem not in new_k:
+        new_k.append(elem)
+store = new_k
+print(*store, sep="\n")
+print("Time taken to complete {} runs:".format(n),two-one)
+
+os.remove('data.txt')
